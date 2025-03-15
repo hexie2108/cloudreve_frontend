be1f21772d610b795ed74ea688f939acad932e75
hexie2108 <hexie2109@gmail.com>
2025-03-15 11:47:39 +0100

    添加电报群链接

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80c38b46ffffb40fa3fef90296d12178.js"
+  "/precache-manifest.598caf58dae1f9d82d95270629bd232a.js"
 );
 
 self.addEventListener('message', (event) => {
