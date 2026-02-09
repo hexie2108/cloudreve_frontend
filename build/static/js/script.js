8dcb118430ea17b8ae653e14db39f34f6ebb484f
hexie2108 <hexie2109@gmail.com>
2026-02-09 19:48:26 +0100

    更新VPN广告链接

--- build/static/js/script.js
+++ build/static/js/script.js
@@ -59,7 +59,7 @@ function print_vpn_image() {
       break;
     //显示千速喵广告
     case 1:
-      link = "https://liltpupu.top/#/register?code=MGIBvSHq";
+      link = "https://eueua.top/#/register?code=ShZKi2js";
       img = "https://cdn2.mikuclub.fun/img/charge/banner_pc_tsmeow.webp";
       img_phone = "https://cdn2.mikuclub.fun/img/charge/banner_phone_tsmeow.webp";
       break;
